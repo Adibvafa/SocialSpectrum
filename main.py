5b760086f5888614d2e549e27a7007087de9cff9
JessicaZhu18 <138637896+JessicaZhu18@users.noreply.github.com>
2023-07-09 00:03:23 -0400

    Update main.py
    Lesson content

--- main.py
+++ main.py
@@ -1,16 +1,72 @@
-# This is a sample Python script.
+lesson_title = "\033[0mUnderstanding and managing ASD individual's emotions\033[0m"
+part1_title = "\033[0mthe Necessity of Emotion Recognition in Individuals with Autism Spectrum Disorder (ASD)\033[0m"
+part1_content = "Autism Spectrum Disorder (ASD) affects a significant portion of the population." \
+                "Understanding and recognizing emotions in individuals with ASD is vital for their " \
+                "well-being and inclusive communities. ASD individuals struggle with perceiving and interpreting " \
+                "emotions, leading to social isolation. Improved emotional recognition enhances social skills" \
+                " and connections. Recognizing emotional needs benefits caregivers, educators, and professionals. " \
+                "Society benefits from understanding ASD emotions, promoting empathy. Emphasizing emotional " \
+                "understanding fosters well-being and inclusion, valuing diverse emotions and supporting every" \
+                " individual, regardless of neurodiversity."
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+part2_title = "\033[0mWays of helping ASD individuals to learn about their emotions -- Everyday Interactions and Emotions Tool\033[0m"
+part2_content = "Autistic individuals often struggle to recognize and understand emotions in themselves and others, " \
+                "especially when upset. Here are some helpful approaches:\n1.	Emotion Pointing: Clearly identify and " \
+                "name emotions, starting with basic ones like happiness, fear, and anger, and gradually progress to " \
+                "more complex emotions like jealousy or frustration. For example, say: I can see you're frustrated. " \
+                "Are you having trouble with that guitar chord?\n" \
+                "2.	Sensation Description: Encourage individuals to describe the physical sensations that accompany " \
+                "their emotions. If someone seems worried, suggest they describe it as a 'blender in their stomach' " \
+                "or acknowledge a faster heartbeat when feeling scared.\n" \
+                "3.	Emotional Recognition in Media: Watch movies or shows together, like 'Inside Out',' " \
+                "and discuss how characters' behaviours reflect their emotions. This helps individuals learn to " \
+                "recognize emotions in others by connecting them to specific expressions or actions.\n" \
+                "4.	Visual Tools: Use visual aids like ladder pictures or thermometers to help individuals gauge and" \
+                " communicate their emotional intensity. Create a ladder with different rungs labelled with numbers " \
+                "or pictures representing anger levels. For example, a smiley face for minimal anger and an enraged " \
+                "face or exploding head emoji for extreme anger.\nBy using these strategies, we can support individuals " \
+                "with ASD in understanding and recognizing their emotions, improving communication and emotional " \
+                "well-being."
 
+part3_title = "\033[0mTechniques for ASD individuals to manage their emotions\033[0m"
+part3_content = "1. Calming down steps: (You can help autistic children and teenagers calm down from strong" \
+                " emotions using a 5-step process)\na. Notice the emotion.\nb. Name the emotion.\nc. Pause and say " \
+                "nothing.\nd. Support your child while they calm down.\ne. Address the issue that sparked the emotion.\n" \
+                "2. Relaxation exercises: Individuals could try some relaxation exercises to see what works for them." \
+                " For example, they could count to 10, take 5 deep breaths, or think about something that makes them" \
+                "happy and calm. They could also try using their fingers to focus on their breathing. " \
+                "Using a finger, your child slowly traces around their hand, breathing in when they trace up to the " \
+                "top of a finger and breathing out when they trace back down a finger. Repeat for all 10 fingers." \
+                "\n3. Sensory stimulation: Individual could clap their hands if they’re excited or squeeze a cushion " \
+                "or a sensory toy if they’re angry. Fidget toys from a sensory kit or stimming might also help." \
+                "If the individual's sensory-seeking behaviour isn’t appropriate, you might be able to replace it " \
+                "with a different behaviour that meets the same sensory need. For example, if your child rocks, " \
+                "they could use a rocking chair instead. If they pick their skin, they could fiddle with rings or " \
+                "a bracelet or pick off clear nail polish instead. \n4. Break time: Individuals could go for a walk," \
+                " get a drink of water, or find a quiet place to sit.\n5. Change of activity: Encourage the individual " \
+                "to take a brain break and listen to their favourite music, read a book or listen to a podcast about" \
+                " their special interests.\n6. Physical activity: Individual could go for a short run, kick a " \
+                "football, do some push-ups or shoot some basketball hoops."
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+part4_title = "\033[0mConclusion\033[0m"
+part4_content = "Understanding and recognizing the emotions of individuals with Autism Spectrum Disorder " \
+                "(ASD) is crucial. Supporting their emotional awareness fosters inclusivity, improves well-being," \
+                " and enhances social interactions. Empowering individuals with ASD to understand their own " \
+                "emotions enable effective communication, self-advocacy, and participation in decision-making. " \
+                "Promoting emotional understanding also creates a society that values neurodiversity, " \
+                "embraces differences, and celebrates unique emotional experiences."
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Printing the lesson structure
+print("Lesson:", lesson_title)
+print()
+print("Part 1:", part1_title)
+print(part1_content)
+print()
+print("Part 2:", part2_title)
+print(part2_content)
+print()
+print("Part 3:", part3_title)
+print(part3_content)
+print()
+print("Part 4:", part4_title)
+print(part4_content)
